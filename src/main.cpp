60e8b248ef5b107ae5d3d548cc7ada0a3a8d4d4e
Alfrheim <marc.badiac@gmail.com>
2019-02-12 14:54:19 +0100

    created a static function to be used in the connect for start.

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,8 @@
 #include <QAction>
 #include <QSystemTrayIcon>
 #include <QIcon>
+#include <chrono>
+#include <thread>
 
 class HvildQWidget : public QWidget {
 
@@ -11,13 +13,26 @@ public:
   HvildQWidget(QWidget *parent = 0);
 };
 
+static void sleep(int delay) {
+  std::this_thread::sleep_for(std::chrono::seconds(delay));
+}
+
+
+static void notify() {
+  system("notify-send 'Starting pomodoro!' ");
+  sleep(5);
+  system("notify-send 'Pomodoro finished, take a breack!' ");
+}
+
 HvildQWidget::HvildQWidget(QWidget *parent)
   : QWidget(parent) {
 
   QAction *startAction;
   QAction *quitAction;
+  
 
   startAction = new QAction(tr("&Start"));
+  connect(startAction, &QAction::triggered, this, notify);
   quitAction = new QAction(tr("&Quit"));
   connect(quitAction, &QAction::triggered, this, &QCoreApplication::quit);
 
